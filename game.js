f49fe205af3f9767005d1bf607f77f51bb840184
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-06-24 17:38:57 -0700

    update map size and scale

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 kaboom({
 	global: true,
 	fullscreen: true,
-	scale: 1,
+	scale: 2,
 	debug: true,
 	clearColor: [0,0,0,1],
   });
@@ -15,6 +15,9 @@ scene("main", () => {
 	layers(['bg', 'obj', 'ui'], 'onj')
 
 	const map = [
+		'                              ',
+		'                              ',
+		'                              ',
 		'                              ',
 		'                              ',
 		'                              ',
